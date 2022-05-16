1dd079ad857c2b22f4cd6568dbcb4e241a58f2b6
wisdomntui <wisdom.ntui@gmail.com>
2022-05-16 10:48:37 +0100

    Create arrays containing filters for users and relatives

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    /**
+     * Gets users based on filters
+     *
+     * @param Request $request
+     */
+    public function getUsers(Request $request)
+    {
+        /**
+         * Here we will get users and their respective relatives based on different
+         * filters for users and relatives.
+         */
+
+        //  User Filters
+        $userFilter = [
+            'age' => 2,
+            'relationship' => 'married',
+            'location' => 'Nairobi',
+        ];
+
+    }
+
 }
