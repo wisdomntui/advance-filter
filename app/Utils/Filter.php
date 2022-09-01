2aa168923368c27017abd18b49b39c92c3562f99
Wisdom Ntui <wisdomntui@gmail.com>
2022-09-01 09:45:14 +0100

    Add a handle method to the Filter class

--- app/Utils/Filter.php
+++ app/Utils/Filter.php
@@ -14,4 +14,16 @@ class Filter{
     public function __construct($query){
         $this->query = $query;
     }
+
+    /**
+     * This method adds each filter value and name to an eloquent where clause
+     * 
+     * @param string $name
+     * @param string $value
+     * 
+     * @return void
+     */
+    public function handle($name, $value){
+        $this->query->where($name, $value);
+    }
 }
