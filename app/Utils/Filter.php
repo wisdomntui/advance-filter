14c093af9081f66eaa98eef372fd7f7054ccfa12
Wisdom Ntui <wisdomntui@gmail.com>
2022-09-01 09:35:31 +0100

    Add constructor to Filter class

--- app/Utils/Filter.php
+++ app/Utils/Filter.php
@@ -4,5 +4,14 @@ namespace App\Utilities;
 
 class Filter{
     protected $query;
-    
+
+
+    /**
+     * Constructor
+     * 
+     * @param $query
+     */
+    public function __construct($query){
+        $this->query = $query;
+    }
 }
